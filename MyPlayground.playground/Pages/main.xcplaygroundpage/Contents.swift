2a08553d1e452fbe63aa99d23995e7755d7a177c
dochonglo <ismaelbarry777@hotmail.com>
2016-07-05 18:04:28 -0400

    Swift Multiple Argument Functions Lab is Complete

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -14,7 +14,12 @@
  ### 1. Create a function that will take in a continent and the number of countries in that continent. It should print the following sentence "____ is a continent which contains ___ countries".
  */
 // write your code here
-
+var continent = "Africa"
+var numberOfCountriesInContinent = 54
+func returnStatment(continent : String, number : Int) {
+    print("\(continent) is a continent which contains \(number) countries.")
+}
+returnStatment(continent, number: numberOfCountriesInContinent)
 
 
 
@@ -25,7 +30,8 @@ func greeting(name: String, greeting: String) {
     print("\(greeting), \(name)!")
 }
 
-greeting("Danny", "Hello")
+// The second parameter needed the external parameter called "greeting"
+greeting("Danny", greeting: "Hello")
 
 
 
@@ -33,9 +39,10 @@ greeting("Danny", "Hello")
 /*: question3
  ### 3. This function doesn't work, either. Can you fix the function (_not_ the call) so that it works?
  */
-func daysInMonth(month: String, days: String) {
+func daysInMonth(month: String, days: Int) {
     print("There are \(days) in \(month)")
 }
+// The function type for days needs to be Int.
 
 daysInMonth("November", days: 30)
 
@@ -47,7 +54,13 @@ daysInMonth("November", days: 30)
  */
 // write your code here
 
-
+var beverage = "Coconut Juice"
+var numberOfBottles = 9
+var placeToKeepBottles = "Refridge"
+func returnFunction(beverage : String, number : Int, place : String) {
+    print("\(beverage) bottles of \(number) on the \(place) wall.")
+}
+returnFunction(beverage, number: numberOfBottles, place: placeToKeepBottles)
 
 
 /*:
